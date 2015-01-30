83f1960ef9f1a0409267c2c3391b412a15199bd3
Adam Waxman <awaxman11@gmail.com>
2015-01-30 15:08:50 -0500

    Add methods to GithubWebhookbsController to add repo, organization, and category to the issue object

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -9,11 +9,33 @@ class GithubWebhooksController < ActionController::Base
     firebase = Firebase::Client.new(base_uri)
     cgi_parsed = CGI::parse(request.raw_post)
     parsed = JSON.parse(cgi_parsed['payload'][0])
-    issue0 = parsed['issue'].to_json
-    issue3 = JSON.parse(issue0)
-    issue1 = issue0.gsub!(/\"/, '\'')
-    issue2 = issue1.slice(1, issue1.length-1)
-    response = firebase.update(parsed["issue"]["id"], issue3)
+    json_issue = parsed['issue'].to_json
+    issue = JSON.parse(json_issue)
+    add_repo_and_org(issue)
+    add_category(issue)
+    response = firebase.update(parsed["issue"]["id"], issue)
     head :ok, content_type: "text/html"
+  end
+
+  def add_repo_and_org(issue)
+    url = issue["url"]
+    url_array = url.split("/")
+    org = url_array[4]
+    repo = url_array[5]
+
+    issue["organization"] = org
+    issue["repo"] = repo
+  end
+
+  def add_category(issue)
+    title = issue["title"]
+    bracket_index = title.index("]")
+    if bracket_index
+      category = title.slice(0..bracket_index.to_i)
+      clean_category = category.delete('[]')
+      issue["category"] = clean_category.downcase
+    else 
+      issue["category"] = ""
+    end
   end
 end
