020a91fdf074a0b13e4560505f8802c510fd1fb4
Adam Waxman <awaxman11@gmail.com>
2015-01-29 22:59:50 -0500

    Get webhook working

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,23 +1,11 @@
 # app/controllers/github_webhooks_controller.rb
 # Rails.application.secrets.key_name
-class GithubWebhooksController < ActionController::API
-  include GithubWebhook::Processor
+class GithubWebhooksController < ActionController::Base
+  # include GithubWebhook::Processor
 
-  def push(payload)
-    # TODO: handle push webhook
-    logger.info "push_payload"
-  end
-
-  def webhook_secret(payload)
-    logger.info "webhook_secret"
-  end
-
-  def issues(payload)
-    logger.info payload
-  end
-
-  def create(payload)
-    logger.info payload
+  def create
+    puts "hello"
+    render json: params
   end
 
 end
